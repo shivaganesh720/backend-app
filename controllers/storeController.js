d7d2473a94fb466b60b41d61f7e7f10d820ab3f7
shivaganesh720 <naguruganesh2005@gmail.com>
2026-03-10 13:36:42 +0530

    added add, edit .ejs files and made some changes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,8 +1,8 @@
 import productModel from "../models/productModel.js";
 
-const showProducts = async (req, res) => {
-    const products = await productModel.find();
-    res.render("store/products", { products });
-};
+const showProducts = async (req,res) => {
+    const products = await productModel.find()
+    res.render("store/products",{products})
+}
 
-export { showProducts }
+export {showProducts}
--- index.js
+++ index.js
@@ -3,18 +3,15 @@ import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-// import {productRouter} from "./routes/productRoute.js";
-import { storeRouter } from "./routes/storeRoute.js";
 import dbConnect from "./config/db.js";
-
-
+import productRouter from "./routes/productRoute.js";
+import { storeRouter } from "./routes/storeRoute.js";
 const app = express();
 dotenv.config();
-
-
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
+app.set("layout", "layout")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -28,16 +25,14 @@ app.use(
 
 app.use("/", storeRouter);
 // app.use("/auth", authRouter);
-// app.use("/products", productRouter);
+app.use("/products", productRouter);
 // app.use("/users", userRouter);
-
 
 const startServer = async () => {
     await dbConnect();
-
     app.listen(5000, () => {
         console.log("Server Started on port 5000");
     });
 };
 
-startServer();
+startServer()
