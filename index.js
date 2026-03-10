a8a6455b18de64fae79c3d267e696df28efb9187
shivaganesh720 <naguruganesh2005@gmail.com>
2026-03-10 18:18:40 +0530

    updated

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set("views", "views");
 app.set("layout", "layout")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+// app.use(cors());
 
 app.use(
     session({
